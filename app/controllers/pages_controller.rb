8e531b3fcbe7b8b7c4d48f4a03a8e3e91d177eaa
trudy macnabb <trudy.macnabb@gmail.com>
2017-07-07 11:58:15 -0400

    helpful knowledge artwork

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,4 +22,40 @@ class PagesController < ApplicationController
 	def contact
 		
 	end
+
+	def hk_artwork_requirements
+		respond_to do |format|
+			format.html {render :template => "/pages/helpful_knowledge/hk_artwork_requirements.html.erb"}
+		end
+	end
+
+	def hk_conversion_tables
+		respond_to do |format|
+			format.html {render :template => "/pages/helpful_knowledge/hk_conversion_tables.html.erb"}
+		end
+	end
+
+	def hk_size_references
+		respond_to do |format|
+			format.html {render :template => "/pages/helpful_knowledge/hk_size_references.html.erb"}
+		end
+	end
+
+	def hk_design_with_type
+		respond_to do |format|
+			format.html {render :template => "/pages/helpful_knowledge/hk_design_with_type.html.erb"}
+		end
+	end
+
+	def hk_choosing_colour
+		respond_to do |format|
+			format.html {render :template => "/pages/helpful_knowledge/hk_choosing_colour.html.erb"}
+		end
+	end
+
+	def hk_educational_videos
+		respond_to do |format|
+			format.html {render :template => "/pages/helpful_knowledge/hk_educational_videos.html.erb"}
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,13 @@ Rails.application.routes.draw do
   get 'services' => 'pages#services'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
+  get 'helpful_knowledge/artwork' => 'pages#hk_artwork_requirements'
+  get 'helpful_knowledge/conversion' => 'pages#hk_conversion_tables'
+  get 'helpful_knowledge/size' => 'pages#hk_size_references'
+  get 'helpful_knowledge/design_type' => 'pages#hk_design_with_type'
+  get 'helpful_knowledge/colour' => 'pages#hk_artwork_requirements'
+  get 'helpful_knowledge/videos' => 'pages#hk_educational_videos'
+
   get 'admin' => 'pages#admin'
 
   post 'sort_pictures' => 'picture#sort' 
